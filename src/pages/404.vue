7a5da214efe2fb3fad75bb8142c391c31d09fe9b
Stephen Ajulu <alunje73@gmail.com>
2020-07-20 18:04:10 +0300

    Create 404.vue

---
+++ src/pages/404.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <h1 class="title">Oops, Page Not Found</h1>
+    <a href="/" class="gohome"><h3>Go Back Home</h3></a>
+    <div class="icon">
+      <g-image src="../assets/images/404.svg"></g-image>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.title {
+align-items: center;
+text-align: center;
+}
+
+.gohome {
+align-items: center;
+text-align: center;
+}
+</style>
