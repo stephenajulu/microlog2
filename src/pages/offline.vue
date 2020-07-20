c8d9753bcd34d3e75a3030929c687950d358cf33
Stephen Ajulu <alunje73@gmail.com>
2020-07-20 18:04:55 +0300

    Create offline.vue

---
+++ src/pages/offline.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <h1 class="title">Oops, You are Offline</h1>
+    <h3 class="offline">No Internet, But You can Still Browse Here</h3>
+    <div class="icon">
+      <g-image src="../assets/images/offline.svg"></g-image>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.title {
+align-items: center;
+text-align: center;
+}
+.offline {
+align-items: center;
+text-align: center;
+}
+</style>
