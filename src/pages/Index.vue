f4fede4453621f3d7541a0578b0cecc4e4bd458f
Stephen Ajulu <alunje73@gmail.com>
2020-07-25 13:56:01 +0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,7 +20,7 @@ query {
         title
         date (format: "D. MMMM YYYY")
         timeToRead
-        description
+        body
         cover_image (width: 770, height: 380, blur: 10)
         ...on Post {
         id
